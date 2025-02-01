820dd3ca11861d61f3e19b37984faa11f352ef46
Pankaj <pankajmehra.seo@gmail.com>
2025-02-01 23:30:30 +0530

    cc

--- src/app/(main)/app/layout.tsx
+++ src/app/(main)/app/layout.tsx
@@ -1,5 +1,5 @@
-import DashboardNavbar from "@/components/dashboard/dashboard-navbar";
-import DashboardSidebar from "@/components/dashboard/dashboard-sidebar";
+// import DashboardNavbar from "@/components/dashboard/dashboard-navbar";
+// import DashboardSidebar from "@/components/dashboard/dashboard-sidebar";
 import React from 'react';
 
 interface Props {
@@ -9,9 +9,9 @@ interface Props {
 const DashboardLayout = ({ children }: Props) => {
     return (
         <div className="flex flex-col min-h-screen w-full">
-            <DashboardNavbar />
+            {/* <DashboardNavbar /> */}
             <main className="flex flex-col lg:flex-row flex-1 size-full">
-                <DashboardSidebar />
+                {/* <DashboardSidebar /> */}
                 <div className="w-full pt-14 lg:ml-72">
                     {children}
                 </div>
--- src/components/global/providers.tsx
+++ src/components/global/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react"
-import { ClerkProvider } from "@clerk/nextjs"
+// import { ClerkProvider } from "@clerk/nextjs"
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface Props {
@@ -13,9 +13,9 @@ interface Props {
 const Providers = ({ children }: Props) => {
     return (
         // <QueryClientProvider client={client}>
-        <ClerkProvider>
+        // <ClerkProvider>
             {children}
-        </ClerkProvider>
+        // </ClerkProvider>
         // </QueryClientProvider> 
     );
 };
